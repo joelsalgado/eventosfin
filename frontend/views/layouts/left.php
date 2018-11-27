3e9325226787854ae090ae61d0787ecaec654fcd
joelsalgado <joelsalgado1302@gmail.com>
2018-11-26 18:29:22 -0600

    iconos left

--- frontend/views/layouts/left.php
+++ frontend/views/layouts/left.php
@@ -7,8 +7,8 @@
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
                     ['label' => 'Menu', 'options' => ['class' => 'header']],
-                    ['label' => 'Usuarios', 'icon' => 'user', 'url' => ['/user'], 'visible' => $user = (Yii::$app->user->identity->role == 30) ? true : false],
-                    ['label' => 'Registro', 'icon' => 'dashboard', 'url' => ['/metadato']],
+                    ['label' => 'Usuarios', 'icon' => 'address-book', 'url' => ['/user'], 'visible' => $user = (Yii::$app->user->identity->role == 30) ? true : false],
+                    ['label' => 'Registro', 'icon' => 'user', 'url' => ['/metadato']],
                     ['label' => 'Iniciar Sesión', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
                     ['label' => 'Cerrar Sesión', 'icon' => 'power-off', 'url' => ['/site/logout'],
                         'template' => '<a href="{url}" data-method="post"><span class="glyphicon glyphicon-off"></span>{label}</a>'
